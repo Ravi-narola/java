1df8207220137fcb82e3964e2b0554826104d907
Ravi-narola <ravinarola3334@gmail.com>
2026-07-07 20:53:25 +0530

    Variables Examples

---
+++ VariablesExample.java
@@ -0,0 +1,38 @@
+public class VariablesExample {
+    public static void main(String[] args){
+
+    // Real-Life Examples
+
+        // Student data
+            String StudentName = "Ravi";
+            int StudentID = 1;
+            int StudentAge = 21;
+            float StudentFee = 5000.50f;
+            char StudentGrade = 'A';
+
+        // Print Student data
+            System.out.println("Student Name: " + StudentName);
+            System.out.println("Student ID: " + StudentID);
+            System.out.println("Student Age: " + StudentAge);
+            System.out.println("Student Fee: " + StudentFee);
+            System.out.println("Student Grade: " + StudentGrade);
+
+
+        System.out.println("\n=========================\n");
+
+    // Calculate the Area of a Rectangle
+
+        // Create integer variables
+            int length = 4;
+            int width = 6;
+            int area;
+
+        // Calculate area
+            area = length * width;
+
+        // Print variables
+            System.out.println("Length is: " + length);
+            System.out.println("Width is: " + width);
+            System.out.println("Area of the rectangle is: " + area);
+    }
+}
