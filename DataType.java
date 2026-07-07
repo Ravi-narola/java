bb53860345039ac2a2e46e690b0411db644c54a3
Ravi-narola <ravinarola3334@gmail.com>
2026-07-07 21:10:30 +0530

    Data Type

---
+++ DataType.java
@@ -0,0 +1,29 @@
+public class DataType {
+    public static void main(String[] args){
+
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+
+        // Print variables
+        System.out.println("Integer: " + myNum);
+        System.out.println("Float: " + myFloatNum); 
+        System.out.println("Character: " + myLetter);
+        System.out.println("Boolean: " + myBool);
+        System.out.println("String: " + myText);
+
+        // Primitive data types
+
+            // includes byte, short, int, long, float, double, boolean and char
+        
+        // Non-primitive data types 
+
+            // such as String, Arrays and Classes (objects)
+            // can be used to call methods to perform certain operations, while primitive data types cannot
+            // are created by the programmer and is not defined by Java (except for String)
+            // can be used to call methods to perform certain operations, while primitive data types cannot
+
+    }
+}
